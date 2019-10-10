830b9d862845bff91f226b1c476826b531fe827e
ImmLearning <51955196+ImmLearning@users.noreply.github.com>
2019-10-10 17:20:52 +0530

    Update installJenkins.sh

--- installJenkins.sh
+++ installJenkins.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 yum update -y && upgrade -y
 java -version
-yum install java-1.8.0
+yum install -y java-1.8.0
 #optional 
 #sudo /usr/sbin/alternatives --config java
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key 
-yum install jenkins
+yum install -y jenkins
 echo "jenkins installed now starting"
 sleep 100
 #sudo 
